a709e4fab182f139987aa4664bf1d8d04de5cd96
turch <vturchannikova@gmail.com>
2025-05-12 20:27:06 +0300

    Created Exceptions

--- src/main/java/experiments/Exceptions.java
+++ src/main/java/experiments/Exceptions.java
@@ -1,4 +1,32 @@
 package experiments;
 
 public class Exceptions {
+    public static void main(String[] args) {
+        String[] strArray = {"str1", "str2","str3"};
+        uncheckedException(strArray);
+        try {
+            pause();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        uncheckedException(strArray);
+
+    }
+
+    private static void uncheckedException (String[] strArray) {
+        try {
+            for (int i = 0; i < 5; i++)
+                System.out.println(strArray[i]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+            System.out.println("create exception");
+        }
+        System.out.println("program is working");
+    }
+
+    private static void pause() throws InterruptedException {
+        Thread.sleep(3000);
+    }
+
 }
+
